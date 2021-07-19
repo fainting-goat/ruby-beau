02f513c808645ed78b229a50648111dbf120a7a6
Kelsey Shannahan <kelsey.shannahan@gmail.com>
2021-07-18 20:57:42 -0400

    Generates a placeholder report

--- app/helpers/report_generator.rb
+++ app/helpers/report_generator.rb
@@ -1,14 +1,23 @@
 module ReportGenerator
   include FileParser
   DEFAULT_FILE = 'input.csv'
-  DEFAULT_REPORT = 'output.csv'
+  DEFAULT_OUTPUT = 'output.csv'
+  DEFAULT_REPORT = 'report.txt'
 
   # can accept a dynamic filename, but defaults to the one provided in the exercise
-  def create_report(filename = DEFAULT_FILE, report_filename = DEFAULT_REPORT)
+  def create_output(filename = DEFAULT_FILE, report_filename = DEFAULT_OUTPUT)
     file_contents = get_file_contents(filename)
     cleaned_contents = clean_contents(file_contents)
     convert_to_csv(cleaned_contents, report_filename)
   end
+
+  def create_report(filename = DEFAULT_REPORT)
+    File.open(filename, 'w+') do |f|
+      f.puts "Everything processed fine because there's no error handling yet!"
+    end
+  end
+
+  private
 
   def convert_to_csv(contents, filename)
     File.open(filename, 'w+') do |f|
--- spec/helpers/report_generator_spec.rb
+++ spec/helpers/report_generator_spec.rb
@@ -1,16 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe ReportGenerator do
-  let(:expected_results) {
-    "Beau,is,the,best,best,best,\nhe,is,just,super,super,super,\n"
-  }
-
-  describe 'create_report' do
+  describe 'create_output' do
     let(:filename) { 'spec/fixtures/test_file.txt' }
-    let(:created_file) { 'spec/fixtures/report.csv' }
+    let(:created_file) { 'spec/fixtures/output.csv' }
+    let(:expected_results) {
+      "Beau,is,the,best,best,best,\nhe,is,just,super,super,super,\n"
+    }
 
     it 'returns the file contents' do
-      create_report(filename, created_file)
+      create_output(filename, created_file)
+      expect(File.read(created_file)).to eq(expected_results)
+    end
+  end
+
+  describe 'create_report' do
+    let(:created_file) { 'spec/fixtures/report.txt' }
+    let(:expected_results) {
+      "Everything processed fine because there's no error handling yet!\n"
+    }
+    it 'returns the file contents' do
+      create_report(created_file)
       expect(File.read(created_file)).to eq(expected_results)
     end
   end
